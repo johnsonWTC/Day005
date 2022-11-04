fedbe525e99cd66d48e5794fdd78ecf8f17b106a
/ohnsonWTC <johnsondubula@gmail.com>
2022-11-04 12:10:19 +0200

    revision

---
+++ src/app/shoe/shoe.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shoe',
+  templateUrl: './shoe.component.html',
+  styleUrls: ['./shoe.component.css']
+})
+export class ShoeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  passedValue :string = 'hey'
+
+  ChangeValue(){
+    alert(this.passedValue)
+  }
+
+}
